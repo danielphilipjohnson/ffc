f2a530cbaf810c4f943b934160d377612b51da81
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2020-07-07 19:24:06 +0100

    Restructure project

--- spec/parseProfileName-spec.js
+++
@@ -1,8 +0,0 @@
-#!javascript
-var profile = require("../dev/js//addProfileName");
-describe("parseProfileName", function () {
-  it("empty returns error", function () {
-    //var product = addProfileName(name);
-    expect(function(){profile.addProfileName("")}).toThrow();
-  });
-});
